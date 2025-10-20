82bac2d37abb4b207bef3b6a2d0c289437a44f47
Serebr1k <serebr11k@gmail.com>
2025-10-20 11:00:34 +0300

    added random balls spawn

--- main/main.gd
+++ main/main.gd
@@ -4,9 +4,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var balls = []
+	for i in range(10):
+		balls.append(cosmic_body.instantiate())
+		balls[i].position = Vector2(randi_range(100, 1200), randi_range(100, 600))
+		add_child(balls[i])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	handle_gravity(delta)
+	
+func handle_gravity(delta):
 	pass
