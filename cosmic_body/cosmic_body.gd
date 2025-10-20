98236f8eb197e8800d67a8ec0f20367733dc243f
Bites_the_gast <denis.syrovenko@gmail.com>
2025-10-20 12:50:35 +0300

    test version of temperatures

--- cosmic_body/cosmic_body.gd
+++ cosmic_body/cosmic_body.gd
@@ -1,6 +1,23 @@
 extends RigidBody2D
 
+class_name Thermal_body
+
+#signals
 signal apply_impulse(vec)
+
+#onready
+
+#const
+
+#vars
+var temperature: float = 2.7  # Start at cosmic background temp (Kelvin)
+var specific_heat: float = 1000.0
+var emissivity: float = 0.9  # 0-1, how well it emits radiation
+var albedo: float = 0.3 # 0-1, how much light it reflects
+#only for planets:
+var atmosphere_thickness: float = 1.0  # 0-1, affects greenhouse effect
+var rotation_speed: float = 1.0
+var current_angle: float = 0.0
 
 func _ready() -> void:
 	pass
@@ -12,3 +29,20 @@ func _process(delta: float) -> void:
 
 func _on_apply_impulse(vec: Vector2) -> void:
 	apply_central_impulse(vec)
+
+func update_visual():
+	pass
+	#if temperature < 100:
+		#color = Color.DARK_BLUE
+	#elif temperature < 200:
+		#color = Color.BLUE
+	#elif temperature < 273:
+		#color = Color.CYAN
+	#elif temperature < 300:
+		#color = Color.GREEN
+	#elif temperature < 500:
+		#color = Color.YELLOW
+	#elif temperature < 1000:
+		#color = Color.ORANGE
+	#else:
+		#color = Color.RED
--- main/main.gd
+++ main/main.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
+#signals
+
+#onready
 @onready var cosmic_body = preload("res://cosmic_body/cosmic body.tscn")
 
-# Called when the node enters the scene tree for the first time.
+#const
+const STEFAN_BOLTZMANN: float = 5.67e-8
+
+#vars
+var thermal_bodies: Array[Thermal_body] = []
+
 func _ready() -> void:
 	var balls = []
 	for i in range(10):
@@ -11,9 +19,41 @@ func _ready() -> void:
 		add_child(balls[i])
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	handle_gravity(delta)
+	handle_temperature(delta)
 	
 func handle_gravity(delta):
 	pass
+
+func handle_temperature(delta):
+	var energy_received: Dictionary = {}
+	for body in thermal_bodies:
+		energy_received[body] = 0.0
+	# Update temperatures
+	for body in thermal_bodies:
+		var area = PI * body.radius * body.radius
+		
+		# Energy lost to space
+		var energy_lost = STEFAN_BOLTZMANN * body.emissivity * area * pow(body.temperature, 4) * delta
+		
+		# Net energy change
+		var net_energy = energy_received[body] - energy_lost
+		
+		# Temperature change: ΔT = Q / (m * c)
+		var delta_temp = net_energy / (body.mass * body.specific_heat)
+		
+		# Update temperature with some damping for stability
+		body.temperature += delta_temp * 0.1
+		
+		# Prevent absolute zero
+		body.temperature = max(2.7, body.temperature)
+		
+		body.update_visual()
+
+func _on_child_entered_tree(node: Node) -> void:
+	thermal_bodies.append(node)
+
+
+func _on_child_exiting_tree(node: Node) -> void:
+	thermal_bodies.erase(node)
